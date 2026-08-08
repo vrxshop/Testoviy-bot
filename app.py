e507c1441f09fea3acedf0ab821048c6d20608a1
vrxshop <testgmeail@gmail.com>
2026-08-08 22:37:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ SUPPORT_CONTACT_EN = "https://t.me/kasgd"
 ADMIN_IDS = [8370080332, 8559381302, 8924977674]
 
 # CRYPTOBOT
-CRYPTOBOT_API_KEY = os.getenv("CRYPTO_TOKEN")
+CRYPTOBOT_API_KEY = "619753:AARNUaqrGF5i0ECxbjejwjmiHBnHXTznaaI"
 CRYPTOBOT_API_URL = "https://api.crypt.bot/v1/"
 
 # ==================================================
